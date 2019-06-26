d1b06f3da965283bc659fd6256426171e4347fc2
Yawei Ge <gyw110399@hotmail.com>
2019-06-25 21:09:11 -0500

    documentation set up

--- R/StatPcp.R
+++ R/StatPcp.R
@@ -1,27 +1,49 @@
-# StatPcp, with a lot of temp modifications
-
-# StatPcp function
-
-# Expected input:
-
-# x: variable names
-# y: corresponding values
-
-# Expected output:
-
-# for geom_segment, we need x(xstart), y(ystart), xend, yend
-# for geom_ribbon, we need x, ymin, ymax
-
-# question list:
-# can I use dplyr inside functions? How? # HH: as dependencies - moved into DESCRIPTION
-# is is.numeric() safe? ## HH: is.numeric shouldn't be used in compute_panel - that's what the default function is doing, we can circumvent the default by implementing our own compute_layer function. That function should probably do most of the calculation
-# do I need to use like spread_ instead of spread?
-# how to get the proper data that we can know if a variable is factor or numeric from  ## function class gives you that info
-
-# for stat_pcp?
-library(ggplot2)
-library(tidyr)
-library(dplyr)
+#' Parallel coordinate plot for both numeric and categorical data
+#'
+#' The parallel coordinate plot displays multiple y-axes, and shows the observations across
+#' several dimensions as ploi-lines. This function work well with both numeric and categorical
+#' variables at the same time after proper scaling.
+#'
+#' @param mapping Set of aesthetic mappings created by [aes()] or
+#'   [aes_()]. If specified and `inherit.aes = TRUE` (the
+#'   default), it is combined with the default mapping at the top level of the
+#'   plot. You must supply `mapping` if there is no plot mapping.
+#' @param data The data to be displayed in this layer. There are three
+#'    options:
+#'
+#'    If `NULL`, the default, the data is inherited from the plot
+#'    data as specified in the call to [ggplot()].
+#'
+#'    A `data.frame`, or other object, will override the plot
+#'    data. All objects will be fortified to produce a data frame. See
+#'    [fortify()] for which variables will be created.
+#'
+#'    A `function` will be called with a single argument,
+#'    the plot data. The return value must be a `data.frame`, and
+#'    will be used as the layer data.
+#' @param geom The geometric object to use display the data
+#' @param position Position adjustment, either as a string, or the result of
+#'    a call to a position adjustment function.
+#' @param na.rm If `FALSE`, the default, missing values are removed with
+#'    a warning. If `TRUE`, missing values are silently removed.
+#' @param show.legend logical. Should this layer be included in the legends?
+#'   `NA`, the default, includes if any aesthetics are mapped.
+#'   `FALSE` never includes, and `TRUE` always includes.
+#'   It can also be a named logical vector to finely select the aesthetics to
+#'   display.
+#' @param inherit.aes If `FALSE`, overrides the default aesthetics,
+#'   rather than combining with them. This is most useful for helper functions
+#'   that define both data and aesthetics and shouldn't inherit behaviour from
+#'   the default plot specification, e.g. [borders()].
+#' @param ... Other arguments passed on to [layer()]. These are
+#'    often aesthetics, used to set an aesthetic to a fixed value, like
+#'    `colour = "red"` or `size = 3`. They may also be parameters
+#'    to the paired geom/stat.
+#'
+#' @import ggplot2
+#' @importFrom dplyr %>% group_by ungroup arrange
+#' @importFrom tidyr spread
+#' @export stat_pcp
 
 stat_pcp <- function(mapping = NULL, data = NULL,
                      geom = "segment", position = "identity",
--- R/geom_pcp.R
+++ R/geom_pcp.R
@@ -1,4 +1,47 @@
-# Geom function of geom_pcp
+#' Add images to plot
+#'
+#' Work like geom_point with points replaced by a predefined image
+#'
+#' @param mapping Set of aesthetic mappings created by [aes()] or
+#'   [aes_()]. If specified and `inherit.aes = TRUE` (the
+#'   default), it is combined with the default mapping at the top level of the
+#'   plot. You must supply `mapping` if there is no plot mapping.
+#' @param data The data to be displayed in this layer. There are three
+#'    options:
+#'
+#'    If `NULL`, the default, the data is inherited from the plot
+#'    data as specified in the call to [ggplot()].
+#'
+#'    A `data.frame`, or other object, will override the plot
+#'    data. All objects will be fortified to produce a data frame. See
+#'    [fortify()] for which variables will be created.
+#'
+#'    A `function` will be called with a single argument,
+#'    the plot data. The return value must be a `data.frame`, and
+#'    will be used as the layer data.
+#' @param stat The statistical transformation to use on the data for this
+#'    layer, as a string.
+#' @param position Position adjustment, either as a string, or the result of
+#'    a call to a position adjustment function.
+#' @param na.rm If `FALSE`, the default, missing values are removed with
+#'    a warning. If `TRUE`, missing values are silently removed.
+#' @param show.legend logical. Should this layer be included in the legends?
+#'   `NA`, the default, includes if any aesthetics are mapped.
+#'   `FALSE` never includes, and `TRUE` always includes.
+#'   It can also be a named logical vector to finely select the aesthetics to
+#'   display.
+#' @param inherit.aes If `FALSE`, overrides the default aesthetics,
+#'   rather than combining with them. This is most useful for helper functions
+#'   that define both data and aesthetics and shouldn't inherit behaviour from
+#'   the default plot specification, e.g. [borders()].
+#' @param ... Other arguments passed on to [layer()]. These are
+#'    often aesthetics, used to set an aesthetic to a fixed value, like
+#'    `colour = "red"` or `size = 3`. They may also be parameters
+#'    to the paired geom/stat.
+#'
+#' @import ggplot2
+#' @export geom_pcp
+
 geom_pcp <- function(mapping = NULL, data = NuLL,
                      # where was "boxplot" created, does the following work?
                      stat = "pcp", position = "identity",
